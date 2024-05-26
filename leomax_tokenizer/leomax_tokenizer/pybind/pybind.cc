e74b3314ad4393adbaf4c7f1cdcc0c54961c45aa
xiongxinlei <xiongxinlei@baidu.com>
2024-05-26 21:51:32 +0800

    添加pybind11.cc可执行的代码

--- leomax_tokenizer/leomax_tokenizer/pybind/pybind.cc
+++ leomax_tokenizer/leomax_tokenizer/pybind/pybind.cc
@@ -1,4 +1,19 @@
 #include <pybind11/pybind11.h>
+#include <Python.h>
 namespace leomax_tokenizer {
+namespace pybind {
 
+namespace py = pybind11;
+
+int add(int i, int j) {
+  return i + j;
 }
+PYBIND11_MODULE(core_tokenizers, m) {
+    m.doc() = "pybind11 example plugin";
+    m.def("add", &add, "A function which adds two number");
+}
+
+} // namespace pybind
+} // namespace leomax_tokenizer
+
+
--- leomax_tokenizer/python/tests/run_test.py
+++ leomax_tokenizer/python/tests/run_test.py
@@ -1,4 +1,6 @@
 from leomax_tokenizer.tokenizer_impl import SentencePieceBPELeoMaxTokenizer
+import leomax_tokenizer
 if __name__ == '__main__':
-    tokenizer = SentencePieceBPELeoMaxTokenizer(vocab='vocab.json',
-                                                merges='merges.txt',)
+    # tokenizer = SentencePieceBPELeoMaxTokenizer(vocab='vocab.json',
+    #                                             merges='merges.txt',)
+    print(leomax_tokenizer.core_tokenizers.add(1, 2))
